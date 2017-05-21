585b2dcdb46aba9db3a64e07134c72a830460c3f
SiruoLiu <1143398076@qq.com>
2017-05-21 12:07:05 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -2,7 +2,7 @@ module.exports = function main(email, suffixes) {
   // Write your code here
   var e=email;
   var s=suffixes[0];
-  if(e.indexOf(s)!=0)
+  if(e.indexOf(s)>=0)
     return true;
   else  
     return false;
